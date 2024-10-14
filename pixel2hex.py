c878c7a2a75bb34074d7260ac00c9abd2cba36d3
mrsandy1965 <123884782+mrsandy1965@users.noreply.github.com>
2024-10-14 15:46:17 +0530

    Update pixel2hex.py
    prettify the code and also made the code more efficent

--- pixel2hex.py
+++ pixel2hex.py
@@ -1,84 +1,77 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import os
 from PIL import Image
 
 # Path to images folder.
-# Relative to folder in which the script is in.
 image_path = "images"
 
 def color_to_bit_string(c, nbits):
-    newc = bin(int(round(c/(256//2**nbits))))
-    bits = newc[2:]
-    return bits.zfill(nbits)
+    """Convert color value to a bit string of specified length."""
+    newc = bin(int(round(c / (256 // 2 ** nbits))))
+    return newc[2:].zfill(nbits)
 
-# Check if images path exists.
 def images_folder_exists():
-    if os.path.exists(image_path):
-        return True
-    else:
-        return False
+    """Check if the images folder exists."""
+    return os.path.exists(image_path)
 
-def is_valid_image(f):
+def is_valid_image(file_name):
+    """Check if the file is a valid image."""
     try:
-        file = Image.open(image_path+"/"+f)
+        with Image.open(os.path.join(image_path, file_name)) as img:
+            return img
     except IOError:
-        print f + " is not an image"
-        return False
-    return file
-        
-def main():  
-    # Output file
-    openfile = open('output.txt', 'w')
-    # Total images converted
+        print(f"{file_name} is not a valid image.")
+        return None
+
+def process_image(img, file_name, openfile):
+    """Process the image and write the converted data to the output file."""
+    pix = img.load()
+    width, height = img.size
+
+    for y in range(height):
+        for x in range(width):
+            if img.mode == "RGBA":
+                r, g, b, a = pix[x, y]
+            else:
+                r, g, b = pix[x, y]
+                a = 255  # Assume fully opaque if not RGBA
+
+            r_bits = color_to_bit_string(r, 3)
+            g_bits = color_to_bit_string(g, 3)
+            b_bits = color_to_bit_string(b, 2)
+            num = int(r_bits + g_bits + b_bits, 2)
+
+            if img.mode == "RGBA" and a == 0:  # If pixel is transparent
+                openfile.write('x"TR",')
+            else:
+                openfile.write(f'x"{num:02x}",')
+
+        if y == 7:  # Add file name as comment on row 7
+            openfile.write(f"    -- {file_name}")
+        openfile.write("\n")
+    openfile.write("\n")  # Empty line after image has been converted
+    print(f"Successfully wrote {file_name} to output.")
+
+def main():
+    """Main function to convert images."""
     total_images = 0
 
-        
-    # Runs for every png in images folder.
-    for f in sorted(os.listdir(os.getcwd()+"/"+image_path)): 
-        img = is_valid_image(f)
-        if img:
-            pix = img.load()
-            width, height = img.size
-            file_name = f.split(".")[0]
-            
-            # Loops through entire image
-            for y in range(height):
-                for x in range(width):
-                    if (img.mode == "RGBA"):
-                        r, g, b, a = pix[x, y]
-                    else:
-                        r, g, b = pix[x, y]
-                    r_bits = color_to_bit_string(r, 3)
-                    g_bits = color_to_bit_string(g, 3)
-                    b_bits = color_to_bit_string(b, 2)
-                    num = int(r_bits + g_bits + b_bits, 2)
-                    '''
-                     for filetypes that do not support transparency,
-                     you will need to check for a certain color if
-                     you need transparency
-                    '''
-                    if (img.mode == "RGBA" and a == 0): #if pixel transparent
-                        openfile.write('x"TR"'+",")
-                    else:
-                        openfile.write('x"{0:02x}"'.format(num) + ",")
-                # adds the file name as comment on row 7
-                if (y == 7):
-                    if (img.mode == "RGBA" and a == 0): #if pixel transparent
-                        openfile.write("    -- %s" % file_name)
-                    else:
-                        openfile.write("    -- %s" % file_name)
-                openfile.write("\n")
-            openfile.write("\n") #empty line after image has been converted
-            print "Successfully wrote %s to output" % f
-            total_images += 1
-    if(total_images == 0):
-        print "No images in folder. 0 Images converted."
-    else:
-        print "Finished converting %s images" % total_images
+    with open('output.txt', 'w') as openfile:
+        for file_name in sorted(os.listdir(image_path)):
+            img = is_valid_image(file_name)
+            if img:
+                process_image(img, file_name.split(".")[0], openfile)
+                total_images += 1
 
-if images_folder_exists():
-    main()
-else:
-    os.makedirs(image_path)
-    print '\033[91m'+"Image folder does not exist. \nAn empty folder, 'images', has been created, add your images in it and rerun the script."
+    if total_images == 0:
+        print("No images in folder. 0 Images converted.")
+    else:
+        print(f"Finished converting {total_images} images.")
+
+if __name__ == "__main__":
+    if images_folder_exists():
+        main()
+    else:
+        os.makedirs(image_path)
+        print('\033[91m' + "Image folder does not exist. An empty folder, 'images', has been created. Please add your images and rerun the script.")
